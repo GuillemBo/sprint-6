fef72d6c145e218a39318daea5c8b06b5a3aec9b
Guillem <guillemb4b@gmail.com>
2024-07-13 10:41:05 +0200

    Test failure correction

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { BudgetService } from '../services/budget.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -10,8 +13,20 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, HomeComponent],
-      providers: [FormBuilder, BudgetService]
+      imports: [ReactiveFormsModule, HomeComponent, RouterTestingModule],
+      providers: [
+        FormBuilder,
+        BudgetService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              numPages: '1',
+              numLanguages: '1'
+            })
+          }
+        }
+      ]
     }).compileComponents();
   });
 
@@ -27,23 +42,22 @@ describe('HomeComponent', () => {
   });
 
   it('should calculate total price correctly', () => {
-
     const budgets = [
-      { title: 'Seo', description: 'Programación web completa', price: 300, controlName: 'Seo' },
-      { title: 'Ads', description: 'Programación web completa', price: 400, controlName: 'Ads' },
-      { title: 'Web', description: 'Programación web completa', price: 500, controlName: 'Web' }
+      { title: 'Seo', description: 'Programación web completa', price: 300, controlName: 'seo' },
+      { title: 'Ads', description: 'Programación web completa', price: 400, controlName: 'ads' },
+      { title: 'Web', description: 'Programación web completa', price: 500, controlName: 'web' }
     ];
     spyOn(budgetService, 'getServices').and.returnValue(budgets);
 
     component.ngOnInit();
 
     component.budgetForm.patchValue({
-      'Seo': true,
-      'Ads': true
+      'seo': true,
+      'ads': true
     });
 
     component.calculateTotalPrice();
 
-    expect(component.totalPrice).toEqual(700); // 300 + 400
+    expect(component.totalPrice).toEqual(700);
   });
 });
